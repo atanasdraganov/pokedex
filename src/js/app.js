d6e1032279b0adc235cda3c9da0f58596eeb683d
Atanas <atanasdraganov77@gmail.com>
2022-12-06 03:58:19 +0200

    failed to connect to api

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,26 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
+  const ul = document.querySelector('ul');
+  const list = document.createDocumentFragment();
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
+
+  fetch(url)
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+
+      let pokemons = data;
+      pokemons?.map(function(pokemon){
+        let li = document.createElement('li');
+        let name = document.createElement('h2');
+
+        name.innerText= `${pokemon.name}`;
+
+        li.appendChild(name);
+        list.appendChild(li);
+      });
+    })
+    ul.appendChild(list);
 });
